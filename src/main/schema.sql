48d07c0b4a141551b3ece93abe963ee4a53c1d45
Miantsa <hei.miantsa.3@gmail.com>
2025-12-19 19:18:45 +0300

    base

---
+++ src/main/schema.sql
@@ -0,0 +1,15 @@
+create type dish_type as enum ('START','MAIN','DESSERT');
+create type category as enum ('VEGETABLE','ANIMAL','MARINE','DAIRY','OTHER');
+
+create table Dish (
+    id serial primary key ,
+    name varchar(255) not null ,
+    dish_type dish_type not null
+);
+
+create table Ingredient (
+    id serial primary key ,
+    name varchar(255) not null,
+    category category not null ,
+    id_dish int references dish(id)
+);

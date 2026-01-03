81fb9b34f0f8317ed7cb7b20dfedf977edeee511
Miantsa <hei.miantsa.3@gmail.com>
2026-01-03 21:30:46 +0300

    Base connection

--- src/main/java/org/example/DBConnection.java
+++ src/main/java/org/example/DBConnection.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private final String URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
+    private final String USER = "mini_dish_db_manager";
+    private final String PASSWORD = "123456";
+
+    public Connection getConnection() {
+        try{
+            return DriverManager.getConnection(URL,USER,PASSWORD);
+        }catch(SQLException e){
+            System.out.println("échec de connection :" +  e.getMessage());
+            return null;
+        }
+    }
 }
